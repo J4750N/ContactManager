e696e2f77de91b7bdb0f2b93352171530aec7455
Arianne Bonneau <1830408@champlaincollege.qc.ca>
2020-12-02 13:43:02 -0500

    list of contacts and buttons

--- ContactManager/MainWindow.xaml.cs
+++ ContactManager/MainWindow.xaml.cs
@@ -23,6 +23,55 @@ namespace ContactManager
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Contact> contactList = new List<Contact>();
+
+            contactList.Add(new Contact() { Name = "Arianne Bonneau", Phone = "450-893-8976", Email = "abonneau@gmail.com" });
+            contactList.Add(new Contact() { Name = "Anthony Toner", Phone = "514-782-3847", Email = "atoner@gmail.com" });
+            contactList.Add(new Contact() { Name = "Jayson Taylor", Phone = "450-738-9090", Email = "jtaylor@gmail.com" });
+            contactList.Add(new Contact() { Name = "Anne Drolet", Phone = "438-273-4546", Email = "adrolet@gmail.com" });
+            contactList.Add(new Contact() { Name = "André Leblanc", Phone = "438-281-6732", Email = "aleblanc@gmail.com" });
+
+            cDataBinding.ItemsSource = contactList;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
+
+    public class toDoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.Phone ;
+        }
+    }
+
 }
